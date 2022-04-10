cc58c0da35c4b229802448180ffa062580e38b6d
Andre Sales <asalesg@gmail.com>
2022-04-10 00:09:33 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+//Som do Pom
+function tocaSom(idElementoAudio) {
+   document.querySelector(idElementoAudio).play();
+}
+/* comentarios maiores */
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0
+
+while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1]
+    //tamplate string
+    const idAudio = `#som_${instrumento}`
+    tecla.onclick = function (){
+        tocaSom(idAudio)
+    }
+    contador = contador + 1
+}
